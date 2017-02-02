d6c08388dde30fb23e7dee3c45986e7ed724a8a7
Charlie Paul <Charlie.Paul@windriver.com>
2017-02-02 15:08:11 -0800

    arch/arm/mach-axxia: Updated to support 4.9 on the 5500
    This updates the axxia code for the 5500 to support linux 4.9
    
    Signed-off-by: Charlie Paul <Charlie.Paul@windriver.com>
    [Quanyang:
    Because of upstream commit ba5d08c0ea78, modify as below:
    1. change "clocksource_probe" to "timer_probe" in axxia_dt_timer_init
    ]
    [Quanyang:Original patch taken from
    git://git.yoctoproject.org/linux-yocto-4.12 standard/axxia/base]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- arch/arm/mach-axxia/axxia.c
+++ arch/arm/mach-axxia/axxia.c
@@ -138,7 +138,7 @@ void __init axxia_dt_timer_init(void)
 	axxia_init_clocks(is_sim);
 
 	of_clk_init(NULL);
-	clocksource_of_init();
+	timer_probe();
 }
 
 static struct mmci_platform_data mmc_plat_data = {
