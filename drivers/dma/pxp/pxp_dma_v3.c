f1e90080b980911dda6f46bd2e04f1a11d7d7d3e
Fancy Fang <chen.fang@nxp.com>
2017-03-03 09:07:20 +0800

    MLK-14314-12 dma: pxp-v3: refine pxp timer init in probe()
    commit  230536b678fee20696c71f629d668566965ba277 from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Extract the timer initialization from pxp_probe()
    to a seperate function call to make probing process
    more clear.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>
    (cherry picked from commit c8a127e506bca2e38fae02c03ceae4e6956d6ea3)
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/dma/pxp/pxp_dma_v3.c
+++ drivers/dma/pxp/pxp_dma_v3.c
@@ -1394,9 +1394,9 @@ static inline void clkoff_callback(struct work_struct *w)
 	pxp_clk_disable(pxp);
 }
 
-static void pxp_clkoff_timer(unsigned long arg)
+static void pxp_clkoff_timer(struct timer_list *t)
 {
-	struct pxps *pxp = (struct pxps *)arg;
+	struct pxps *pxp = from_timer(pxp, t, clk_timer);
 
 	if ((pxp->pxp_ongoing == 0) && list_empty(&head))
 		schedule_work(&pxp->work);
@@ -5223,6 +5223,13 @@ static void pxp_remove_attrs(struct platform_device *pdev)
 	device_remove_file(&pdev->dev, &dev_attr_block_size);
 }
 
+static void pxp_init_timer(struct pxps *pxp)
+{
+	INIT_WORK(&pxp->work, clkoff_callback);
+
+	timer_setup(&pxp->clk_timer, pxp_clkoff_timer, 0);
+}
+
 #ifdef	CONFIG_MXC_FPGA_M4_TEST
 static void pxp_config_m4(struct platform_device *pdev)
 {
@@ -5313,10 +5320,7 @@ static int pxp_probe(struct platform_device *pdev)
 	dump_pxp_reg(pxp);
 	pxp_clk_disable(pxp);
 
-	INIT_WORK(&pxp->work, clkoff_callback);
-	init_timer(&pxp->clk_timer);
-	pxp->clk_timer.function = pxp_clkoff_timer;
-	pxp->clk_timer.data = (unsigned long)pxp;
+	pxp_init_timer(pxp);
 
 	init_waitqueue_head(&pxp->thread_waitq);
 	/* allocate a kernel thread to dispatch pxp conf */
