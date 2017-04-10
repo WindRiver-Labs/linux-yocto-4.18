1f285ee4848d1b88f667dbc2e192324ffa7a3738
Kieran Bingham <kieran.bingham@ideasonboard.com>
2017-04-10 11:24:24 -0700

    uvcvideo: Convert decode functions to use new context structure
    commit  02b73582e5122febe5b342488b779c17cfd62001 from
    https://github.com/Xilinx/linux-xlnx.git
    
    The URB completion handlers currently reference the stream context.
    
    Now that each URB has its own context structure, convert the decode (and
    one encode) functions to utilise this context for URB management.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/media/usb/uvc/uvcvideo.h
+++ drivers/media/usb/uvc/uvcvideo.h
@@ -483,11 +483,13 @@ struct uvc_stats_stream {
  * struct uvc_urb - URB context management structure
  *
  * @urb: described URB. Must be allocated with usb_alloc_urb()
+ * @stream: UVC streaming context
  * @urb_buffer: memory storage for the URB
  * @urb_dma: DMA coherent addressing for the urb_buffer
  */
 struct uvc_urb {
 	struct urb *urb;
+	struct uvc_streaming *stream;
 
 	char *urb_buffer;
 	dma_addr_t urb_dma;
@@ -525,8 +527,7 @@ struct uvc_streaming {
 	/* Buffers queue. */
 	unsigned int frozen : 1;
 	struct uvc_video_queue queue;
-	void (*decode) (struct urb *urb, struct uvc_streaming *video,
-			struct uvc_buffer *buf, struct uvc_buffer *meta_buf);
+	void (*decode) (struct uvc_urb *uvc_urb, struct uvc_buffer *buf, struct uvc_buffer *meta_buf);
 
 	struct {
 		struct video_device vdev;
@@ -795,9 +796,7 @@ struct usb_host_endpoint *uvc_find_endpoint(struct usb_host_interface *alts,
 					    u8 epaddr);
 
 /* Quirks support */
-void uvc_video_decode_isight(struct urb *urb, struct uvc_streaming *stream,
-			     struct uvc_buffer *buf,
-			     struct uvc_buffer *meta_buf);
+void uvc_video_decode_isight(struct uvc_urb *uvc_urb, struct uvc_buffer *buf, struct uvc_buffer *meta_buf);
 
 /* debugfs and statistics */
 void uvc_debugfs_init(void);
