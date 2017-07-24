6984ab610e5de3b2c70735914f7be800c98b03a3
Andrey Gusakov <andrey.gusakov@cogentembedded.com>
2017-07-24 20:22:03 +0300

    pci: pcie-rcar: add regulators support
    commit 78ade93086080c37638af492c567e049a0ee51e7 from
    https://github.com/CogentEmbedded/meta-rcar.git
    
    Add PCIE regulators
    
    Signed-off-by: Andrey Gusakov <andrey.gusakov@cogentembedded.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/pci/controller/pcie-rcar.c
+++ drivers/pci/controller/pcie-rcar.c
@@ -14,6 +14,7 @@
 #include <linux/bitops.h>
 #include <linux/clk.h>
 #include <linux/delay.h>
+#include <linux/regulator/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
 #include <linux/irqdomain.h>
@@ -157,6 +158,8 @@ struct rcar_pcie {
 	struct list_head	resources;
 	int			root_bus_nr;
 	struct clk		*bus_clk;
+	struct regulator	*pcie3v3; /* 3.3V power supply */
+	struct regulator	*pcie1v8; /* 1.8V power supply */
 	struct			rcar_msi msi;
 };
 
@@ -1198,6 +1201,36 @@ static const struct of_device_id rcar_pcie_of_match[] = {
 	{},
 };
 
+static int rcar_pcie_set_vpcie(struct rcar_pcie *pcie)
+{
+	struct device *dev = pcie->dev;
+	int err;
+
+	if (!IS_ERR(pcie->pcie3v3)) {
+		err = regulator_enable(pcie->pcie3v3);
+		if (err) {
+			dev_err(dev, "fail to enable vpcie3v3 regulator\n");
+			goto err_out;
+		}
+	}
+
+	if (!IS_ERR(pcie->pcie1v8)) {
+		err = regulator_enable(pcie->pcie1v8);
+		if (err) {
+			dev_err(dev, "fail to enable vpcie1v8 regulator\n");
+			goto err_disable_3v3;
+		}
+	}
+
+	return 0;
+
+err_disable_3v3:
+	if (!IS_ERR(pcie->pcie3v3))
+		regulator_disable(pcie->pcie3v3);
+err_out:
+	return err;
+}
+
 static int rcar_pcie_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -1215,6 +1248,26 @@ static int rcar_pcie_probe(struct platform_device *pdev)
 
 	pcie->dev = dev;
 	platform_set_drvdata(pdev, pcie);
+
+	pcie->pcie3v3 = devm_regulator_get_optional(dev, "pcie3v3");
+	if (IS_ERR(pcie->pcie3v3)) {
+		if (PTR_ERR(pcie->pcie3v3) == -EPROBE_DEFER)
+			return -EPROBE_DEFER;
+		dev_info(dev, "no pcie3v3 regulator found\n");
+	}
+
+	pcie->pcie1v8 = devm_regulator_get_optional(dev, "pcie1v8");
+	if (IS_ERR(pcie->pcie1v8)) {
+		if (PTR_ERR(pcie->pcie1v8) == -EPROBE_DEFER)
+			return -EPROBE_DEFER;
+		dev_info(dev, "no pcie1v8 regulator found\n");
+	}
+
+	err = rcar_pcie_set_vpcie(pcie);
+	if (err) {
+		dev_err(dev, "failed to set pcie regulators\n");
+		return err;
+	}
 
 	err = pci_parse_request_of_pci_ranges(dev, &pcie->resources, NULL);
 	if (err)
