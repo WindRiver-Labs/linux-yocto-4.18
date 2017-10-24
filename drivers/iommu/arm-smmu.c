ff734e05c69d9e08c2b36b3a08341f6e47cc079e
Nipun Gupta <nipun.gupta@nxp.com>
2017-10-24 19:17:33 +0530

    iommu: arm-smmu: Add support for the fsl-mc bus
    Implement bus specific support for the fsl-mc bus including
    registering the arm_smmu_ops and bus specific device add operations.
    
    Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>
    [Xulin: Original patch taken from NXP LSDK-18.09.]
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -52,6 +52,7 @@
 #include <linux/spinlock.h>
 
 #include <linux/amba/bus.h>
+#include "../staging/fsl-mc/include/mc.h"
 
 #include "io-pgtable.h"
 #include "arm-smmu-regs.h"
@@ -1465,6 +1466,8 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 
 	if (dev_is_pci(dev))
 		group = pci_device_group(dev);
+	else if (dev_is_fsl_mc(dev))
+		group = fsl_mc_device_group(dev);
 	else
 		group = generic_device_group(dev);
 
@@ -2042,6 +2045,10 @@ static void arm_smmu_bus_init(void)
 		pci_request_acs();
 		bus_set_iommu(&pci_bus_type, &arm_smmu_ops);
 	}
+#endif
+#ifdef CONFIG_FSL_MC_BUS
+	if (!iommu_present(&fsl_mc_bus_type))
+		bus_set_iommu(&fsl_mc_bus_type, &arm_smmu_ops);
 #endif
 }
 
