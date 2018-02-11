a74b9bdfc7d23b7ce0bfcc22a6382269a8887f56
Charlie Paul <Charlie.Paul@windriver.com>
2018-02-10 20:29:44 -0800

    arch/arm: arm changes to support the axxia BSP
    These files were changed to support the LSI axxia 5500 board.
    
    Signed-off-by: Charlie Paul <Charlie.Paul@windriver.com>
    Signed-off-by: John Jacques <john.jacques@intel.com>
    Signed-off-by: Daniel Dragomir <daniel.dragomir@windriver.com>
    [Quanyang: Delete superfluous modifications in arch/arm/include/asm/spinlock.h
    and arch/arm/kernel/perf_event_v7.c]
    [Quanyang:Original patch taken from
    git://git.yoctoproject.org/linux-yocto-4.12 standard/axxia/base]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- arch/arm/include/asm/kmap_types.h
+++ arch/arm/include/asm/kmap_types.h
@@ -5,6 +5,15 @@
 /*
  * This is the "bare minimum".  AIO seems to require this.
  */
+#if defined(CONFIG_ARCH_AXXIA) && defined(CONFIG_SMP)
+#if (CONFIG_NR_CPUS > 15)
+/* Prevent overlap between fixmap mapping and CPU vector page for 16th core */
+#define KM_TYPE_NR 15
+#else
 #define KM_TYPE_NR 16
+#endif
+#else
+#define KM_TYPE_NR 16
+#endif
 
 #endif
