2b72781c524dc5c47f2a5be0c341be798ad0d69c
Shengjiu Wang <shengjiu.wang@nxp.com>
2018-02-26 15:12:32 +0800

    MLK-17620-1: hdp: register generic hdmi codec driver
    commit  f3d92ba3c947229259d58621ad46d861028f0d60 from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Register generic hdmi codec driver, and move audio related
    code to an independent file.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>
    Reviewed-by: Sandor Yu <sandor.yu@nxp.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/gpu/drm/imx/hdp/imx-hdp.h
+++ drivers/gpu/drm/imx/hdp/imx-hdp.h
@@ -225,6 +225,6 @@ struct imx_hdp {
 	struct imx_cec_dev cec;
 };
 
-u32 imx_hdp_audio(AUDIO_TYPE type, u32 sample_rate, u32 channels, u32 width);
+void imx_hdp_register_audio_driver(struct device *dev);
 
 #endif
