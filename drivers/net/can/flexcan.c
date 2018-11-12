556e9a920dc6879ec8caee9fc6870a76e613f670
Marc Kleine-Budde <mkl@pengutronix.de>
2018-11-12 15:33:57 +0100

    can: flexcan: flexcan_start_xmit(): fix indention
    This patch fixes the indentio nin flexcan_start_xmit() by alligning the
    code to the opening parenthesis.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    (cherry picked from commit 9dc1ee1184b4177f98001885387c0e268db06c2a)
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -562,9 +562,9 @@ static int flexcan_start_xmit(struct sk_buff *skb, struct net_device *dev)
 	 * Write twice INACTIVE(0x8) code to first MB.
 	 */
 	priv->write(FLEXCAN_MB_CODE_TX_INACTIVE,
-		      &priv->tx_mb_reserved->can_ctrl);
+		    &priv->tx_mb_reserved->can_ctrl);
 	priv->write(FLEXCAN_MB_CODE_TX_INACTIVE,
-		      &priv->tx_mb_reserved->can_ctrl);
+		    &priv->tx_mb_reserved->can_ctrl);
 
 	return NETDEV_TX_OK;
 }
