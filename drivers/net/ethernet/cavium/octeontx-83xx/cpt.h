58266f64887f38ccc9dfc4ab54f359c6f8e539da
Lukasz Bartosik <lbartosik@marvell.com>
2018-12-28 15:22:23 +0300

    octeontx: add 8X domain interface for CPT
    Change-Id: I65ffa2bb4b8f21fc3c325007cabce002ff64fde0
    Signed-off-by: Lukasz Bartosik <lbartosik@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/1780
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    [Kevin: The original patch got from Marvell sdk10.0_19.06]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

---
+++ drivers/net/ethernet/cavium/octeontx-83xx/cpt.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2017 Cavium, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ */
+
+#ifndef __CPT_H__
+#define __CPT_H__
+
+#include <linux/pci.h>
+#include <linux/types.h>
+#include "octeontx.h"
+
+struct cptpf_com_s {
+	int (*create_domain)(u32, u16, u32, struct kobject *kobj);
+	int (*destroy_domain)(u32 id, u16 domain_id, struct kobject *kobj);
+	int (*reset_domain)(u32, u16);
+};
+
+extern struct cptpf_com_s cptpf_com;
+
+#endif /* __CPT_H__ */
+
