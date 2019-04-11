3219bd4df84948c820b8991c4825d2cfca1a740a
Ben Peled <bpeled@marvell.com>
2019-04-11 12:30:19 +0300

    clk: mvebu: add helper file for Armada AP and CP clocks
    Clock drivers for Armada AP and Armada CP use the same function to
    generate unique clock name. A third drivers is coming with the same
    need, so it's time to move this function in a common file.
    
    Add to the ap-dclk to the original commit.
    
    Change-Id: Ie56c7b80f89fedc9920c1eb3011fb50021123cfd
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Signed-off-by: Ben Peled <bpeled@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/7215
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Stefan Chulski <Stefan.Chulski@cavium.com>
    Reviewed-by: Nadav Haklai <Nadav.Haklai@cavium.com>
    [RH: Original patch taken from marvell 88F3720 board support SDK 10.0-PR2003]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/clk/mvebu/ap806-system-controller.c
+++ drivers/clk/mvebu/ap806-system-controller.c
@@ -12,11 +12,11 @@
 
 #define pr_fmt(fmt) "ap806-system-controller: " fmt
 
+#include "armada_ap_cp_helper.h"
 #include <linux/clk-provider.h>
 #include <linux/mfd/syscon.h>
 #include <linux/init.h>
 #include <linux/of.h>
-#include <linux/of_address.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 
@@ -31,18 +31,6 @@ static struct clk_onecell_data ap806_clk_data = {
 	.clks = ap806_clks,
 	.clk_num = AP806_CLK_NUM,
 };
-
-static char *ap806_unique_name(struct device *dev, struct device_node *np,
-			       char *name)
-{
-	const __be32 *reg;
-	u64 addr;
-
-	reg = of_get_property(np, "reg", NULL);
-	addr = of_translate_address(np, reg);
-	return devm_kasprintf(dev, GFP_KERNEL, "%llx-%s",
-			(unsigned long long)addr, name);
-}
 
 static int ap806_syscon_common_probe(struct platform_device *pdev,
 				     struct device_node *syscon_node)
@@ -141,7 +129,7 @@ static int ap806_syscon_common_probe(struct platform_device *pdev,
 	dclk_freq *= 1000 * 1000;
 
 	/* CPU clocks depend on the Sample At Reset configuration */
-	name = ap806_unique_name(dev, syscon_node, "cpu-cluster-0");
+	name = ap_cp_unique_name(dev, syscon_node, "cpu-cluster-0");
 	ap806_clks[0] = clk_register_fixed_rate(dev, name, NULL,
 						0, cpuclk_freq);
 	if (IS_ERR(ap806_clks[0])) {
@@ -149,7 +137,7 @@ static int ap806_syscon_common_probe(struct platform_device *pdev,
 		goto fail0;
 	}
 
-	name = ap806_unique_name(dev, syscon_node, "cpu-cluster-1");
+	name = ap_cp_unique_name(dev, syscon_node, "cpu-cluster-1");
 	ap806_clks[1] = clk_register_fixed_rate(dev, name, NULL, 0,
 						cpuclk_freq);
 	if (IS_ERR(ap806_clks[1])) {
@@ -158,7 +146,7 @@ static int ap806_syscon_common_probe(struct platform_device *pdev,
 	}
 
 	/* Fixed clock is always 1200 Mhz */
-	fixedclk_name = ap806_unique_name(dev, syscon_node, "fixed");
+	fixedclk_name = ap_cp_unique_name(dev, syscon_node, "fixed");
 	ap806_clks[2] = clk_register_fixed_rate(dev, fixedclk_name, NULL,
 						0, 1200 * 1000 * 1000);
 	if (IS_ERR(ap806_clks[2])) {
@@ -167,7 +155,7 @@ static int ap806_syscon_common_probe(struct platform_device *pdev,
 	}
 
 	/* MSS Clock is fixed clock divided by 6 */
-	name = ap806_unique_name(dev, syscon_node, "mss");
+	name = ap_cp_unique_name(dev, syscon_node, "mss");
 	ap806_clks[3] = clk_register_fixed_factor(NULL, name, fixedclk_name,
 						  0, 1, 6);
 	if (IS_ERR(ap806_clks[3])) {
@@ -176,7 +164,7 @@ static int ap806_syscon_common_probe(struct platform_device *pdev,
 	}
 
 	/* SDIO(/eMMC) Clock is fixed clock divided by 3 */
-	name = ap806_unique_name(dev, syscon_node, "sdio");
+	name = ap_cp_unique_name(dev, syscon_node, "sdio");
 	ap806_clks[4] = clk_register_fixed_factor(NULL, name,
 						  fixedclk_name,
 						  0, 1, 3);
@@ -186,7 +174,7 @@ static int ap806_syscon_common_probe(struct platform_device *pdev,
 	}
 
 	/* AP-DCLK(HCLK) Clock is DDR clock divided by 2 */
-	name = ap806_unique_name(dev, syscon_node, "ap-dclk");
+	name = ap_cp_unique_name(dev, syscon_node, "ap-dclk");
 	ap806_clks[5] = clk_register_fixed_rate(dev, name, NULL,
 						0, dclk_freq);
 
