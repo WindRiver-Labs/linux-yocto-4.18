99f788ee8677270671f09e3e9ead78a7d0c79c83
Yangbo Lu <yangbo.lu@nxp.com>
2019-08-30 17:21:57 +0800

    staging: fsl-dpaa2/rtc: update to upstream driver
    commit b30ce5507dd54083bb013437c1a20a431d2d4dad from
    http://source.codeaurora.org/external/qoriq/qoriq-components/linux.git
    
    Update fsl-dpaa2/rtc driver to upstream dpaa2-ptp driver, whose
    upstream commit id is,
    
    8893a84 dpaa2-ptp: add interrupt support
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- drivers/staging/fsl-dpaa2/rtc/dprtc.h
+++ drivers/staging/fsl-dpaa2/rtc/dprtc.h
@@ -19,17 +19,6 @@ struct fsl_mc_io;
 #define DPRTC_MAX_IRQ_NUM			1
 #define DPRTC_IRQ_INDEX				0
 
-/**
- * Interrupt event masks:
- */
-
-/**
- * Interrupt event mask indicating alarm event had occurred
- */
-#define DPRTC_EVENT_ALARM			0x40000000
-/**
- * Interrupt event mask indicating periodic pulse event had occurred
- */
 #define DPRTC_EVENT_PPS				0x08000000
 
 int dprtc_open(struct fsl_mc_io *mc_io,
@@ -41,24 +30,6 @@ int dprtc_close(struct fsl_mc_io *mc_io,
 		u32 cmd_flags,
 		u16 token);
 
-/**
- * struct dprtc_cfg - Structure representing DPRTC configuration
- * @options:	place holder
- */
-struct dprtc_cfg {
-	u32 options;
-};
-
-int dprtc_create(struct fsl_mc_io *mc_io,
-		 u16 dprc_token,
-		 u32 cmd_flags,
-		 const struct dprtc_cfg *cfg,
-		 u32 *obj_id);
-
-int dprtc_destroy(struct fsl_mc_io *mc_io,
-		  u16 dprc_token,
-		  u32 cmd_flags,
-		  u32 object_id);
 
 int dprtc_enable(struct fsl_mc_io *mc_io,
 		 u32 cmd_flags,
@@ -76,36 +47,6 @@ int dprtc_is_enabled(struct fsl_mc_io *mc_io,
 int dprtc_reset(struct fsl_mc_io *mc_io,
 		u32 cmd_flags,
 		u16 token);
-
-int dprtc_set_clock_offset(struct fsl_mc_io *mc_io,
-			   u32 cmd_flags,
-			   u16 token,
-			   int64_t offset);
-
-int dprtc_set_freq_compensation(struct fsl_mc_io *mc_io,
-				u32 cmd_flags,
-				u16 token,
-				u32 freq_compensation);
-
-int dprtc_get_freq_compensation(struct fsl_mc_io *mc_io,
-				u32 cmd_flags,
-				u16 token,
-				u32 *freq_compensation);
-
-int dprtc_get_time(struct fsl_mc_io *mc_io,
-		   u32 cmd_flags,
-		   u16 token,
-		   uint64_t *time);
-
-int dprtc_set_time(struct fsl_mc_io *mc_io,
-		   u32 cmd_flags,
-		   u16 token,
-		   uint64_t time);
-
-int dprtc_set_alarm(struct fsl_mc_io *mc_io,
-		    u32 cmd_flags,
-		    u16 token,
-		    uint64_t time);
 
 int dprtc_set_irq_enable(struct fsl_mc_io *mc_io,
 			 u32 cmd_flags,
@@ -142,23 +83,5 @@ int dprtc_clear_irq_status(struct fsl_mc_io *mc_io,
 			   u16 token,
 			   u8 irq_index,
 			   u32 status);
-
-/**
- * struct dprtc_attr - Structure representing DPRTC attributes
- * @id:		DPRTC object ID
- */
-struct dprtc_attr {
-	int id;
-};
-
-int dprtc_get_attributes(struct fsl_mc_io *mc_io,
-			 u32 cmd_flags,
-			 u16 token,
-			 struct dprtc_attr *attr);
-
-int dprtc_get_api_version(struct fsl_mc_io *mc_io,
-			  u32 cmd_flags,
-			  u16 *major_ver,
-			  u16 *minor_ver);
 
 #endif /* __FSL_DPRTC_H */
